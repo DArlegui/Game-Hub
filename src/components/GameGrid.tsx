8343d748f15afa16dc7344c6274334c35c46e2fb
daniel <arleguidaniel@gmail.com>
2023-11-23 17:07:06 +0900

    Cleaned up Genre and GameCard Design

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,9 +17,9 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4, '2xl': 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, '2xl': 4 }}
         padding="10px"
-        spacing={5}
+        spacing={4}
         // justifyItems={{ base: 'center', '2xl': 'flex-end' }}
       >
         {isLoading &&
