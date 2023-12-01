bb14128a3537aff98c7ac662a55b84fce5f88a2d
daniel <arleguidaniel@gmail.com>
2023-12-02 05:11:18 +0900

    Refreshed and hid API Key for security purposes

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -10,7 +10,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '94c70a9433e34de0a1ab75841c5b563f',
+    key: 'YOUR_API_KEY',
   },
 });
 
